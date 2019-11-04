feb150093d599e067530b2400c3466379b98dfc6
Danielle Matton <danimatton@gmail.com>
2019-11-04 00:22:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,11 @@ end
 
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls = []
-  planeteer_calls.map do |planeteer_calls|
-    puts "#{planeteer_calls.capitalize}!"
+  new_array = []
+  planeteer_calls.map do |p|
+    planeteer_calls << "#{planeteer_calls.capitalize}!"
   end  
+  puts planeteer_calls
 end
 
 def long_planeteer_calls()
